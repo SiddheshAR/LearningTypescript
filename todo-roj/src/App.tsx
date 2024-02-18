4a23a9205f670fd9f8ebd0044a8a3bb68dbe871c
Siddhesh <siddheshfr@gmail.com>
2024-02-18 16:02:20 +0530

    Part 1

---
+++ todo-roj/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+
+const App:React.FC = () =>{
+  return(
+    <div>
+      <h2>great Friend</h2>
+    </div>
+  )
+}
+
+export default App;
